f2e1c78b832d118d676a012bae79f360aaa1a132
socnaift <alfredorivadulla8@gmail.com>
2026-02-27 22:45:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-// CAMBIAR SECCIONES
 function showSection(id){
   document.querySelectorAll(".section").forEach(sec=>{
     sec.classList.remove("active");
   });
 
   document.getElementById(id).classList.add("active");
-}
-
-// LOGIN SIMPLE
-function openLogin(){
-  alert("Sistema de login lo agregamos después 🔥");
 }
